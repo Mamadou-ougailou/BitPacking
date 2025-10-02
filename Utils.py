e3a2b3ed23e3f67d5282e24fc209e34d79ffa712
Mamadou ougailou Diallo <mamadou-ougailou.diallo@etu-unice.fr>
2025-10-02 19:37:41 +0200

    Latency

---
+++ Utils.py
@@ -0,0 +1,24 @@
+import random
+import statistics
+
+def run_with_averaging(bp, array, runs=5):
+    compression_times = []
+    decompression_times = []
+    size = len(array)
+    arrayDec = [0]*size
+
+    for _ in range(runs):
+        compresed_array = bp.compress(array)
+        compression_times.append(float(bp.getCompressionTime()))
+
+        decompressed_array = bp.decompress(arrayDec)
+        decompression_times.append(float(bp.getDecompressionTime()))
+    
+
+    avg_compression_time = statistics.mean(compression_times)
+    avg_decompression_time = statistics.mean(decompression_times)
+
+    return compresed_array, decompressed_array, avg_compression_time, avg_decompression_time
+
+def generate_input(size, max_val):
+    return [random.randint(0, max_val) for _ in range(size)]
